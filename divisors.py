52e2b7ecb196763612818f4d3b35c6f291886422
rrrtyi <rrrtyi04@gachon.ac.kr>
2024-11-10 23:00:03 +0900

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -1,11 +1,9 @@
 import sys
 
-number = int(sys.argv[1])  # 사용자 입력으로 받은 숫자
+number = int(sys.argv[1])
 
-# 약수를 찾기 위해 1부터 number-1까지 반복
-for i in range(1, number):
-    # number가 i로 나누어 떨어지면 (즉, i는 number의 약수)
+for i in range(1, number+1):
     if number % i == 0:
-        print(i, end=" ")  # 약수 출력
+        print(i, end=" ")
 
-print()  # 마지막에 줄바꿈
+print()
